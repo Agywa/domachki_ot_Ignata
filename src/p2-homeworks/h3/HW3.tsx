29ab75c3501631f4763907369937dfe441755e53
Agywa <91954956+Agywa@users.noreply.github.com>
2022-02-21 13:56:39 +0300

    3

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -5,19 +5,17 @@ import {v1} from "uuid";
 // types
 export type UserType = {
     _id: string // need to fix any
-    names: string // need to fix any
+    name: string // need to fix any
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<Array<UserType>>([
-        {_id: v1(), names: "names"},
-    ]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
 
-    const addUserCallback = (names: string) => { // need to fix any
-        let user= {_id: v1(), names: names};
-        let newUsers = [user, ...users];
-        setUsers(newUsers) // need to fix
+    const addUserCallback = (name: string) => { // need to fix any
+        let user= {_id: v1(), name};
+
+        setUsers( [...users ,user]) // need to fix
     }
 
     return (
